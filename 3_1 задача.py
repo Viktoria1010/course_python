c58dc34e5caf6e1a79d03bb2e1870f5e22eb3dab
Виктория <ostrovskayap@bk.ru>
2021-03-01 15:56:34 +0700

    Сделаны задания третьего семинара (JSON)

--- 3_1 задача.py
+++ 3_1 задача.py
@@ -1,15 +1,12 @@
 import json
 
+dict_new = dict()
 with open('3_1 задача.json', 'w') as to_write:
     with open('C:\\Users\\Виктория\\Downloads\\wikidata_1000.json', 'r', encoding='utf-8') as f:
-        dict_new = dict()
-        for line in f.readlines():
-            dictionary = dict()
-            dictionary[line] = json.loads(line)
+        for line in f:
+            data = json.loads(line)
             try:
-                name = dictionary[line]['label']['value']
-                descr = dictionary[line]['description']['value']
-                dict_new[name] = descr
+                dict_new[data["label"]["value"]] = data["description"]["value"]
             except KeyError:
-                dictionary[line]["label"]["value"] = None
+                dict_new[data["label"]["value"]] = None
         to_write.write(json.dumps(dict_new, ensure_ascii=False, indent=4))
--- 3_2 задача.py
+++ 3_2 задача.py
@@ -3,10 +3,10 @@ import json
 with open('3_2 задача.json', 'w') as to_write:
     with open('C:\\Users\\Виктория\\Downloads\\RomeoAndJuliet.json', 'r', encoding='utf-8') as f:
         romeo = json.load(f)
-        for act in range(len(romeo['acts'])):
-            for scene in range(len(romeo['acts'][act]['scenes'])):
+        for act in romeo['acts']:
+            for scene in act['scenes']:
                 set_char = set()
-                for characters in range(len(romeo['acts'][act]['scenes'][scene]['action'])):
-                    set_char.add(romeo['acts'][act]['scenes'][scene]['action'][characters]['character'])
+                for characters in scene['action']:
+                    set_char.add(characters['character'])
                 to_write.write(json.dumps(list(set_char), ensure_ascii=False))
                 to_write.write('\n')
