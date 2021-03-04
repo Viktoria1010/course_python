ef6799368475824dd1c06471c35f48e8b41958c6
Виктория <ostrovskayap@bk.ru>
2021-03-04 11:43:17 +0700

    Сделаны задания третьего семинара (JSON)

--- 3_1 задача.py
+++ 3_1 задача.py
@@ -1,12 +1,13 @@
 import json
 
-dict_new = dict()
 with open('3_1 задача.json', 'w') as to_write:
     with open('C:\\Users\\Виктория\\Downloads\\wikidata_1000.json', 'r', encoding='utf-8') as f:
+        dict_new = dict()
         for line in f:
             data = json.loads(line)
             try:
                 dict_new[data["label"]["value"]] = data["description"]["value"]
             except KeyError:
-                dict_new[data["label"]["value"]] = None
-        to_write.write(json.dumps(dict_new, ensure_ascii=False, indent=4))
+                dict_new[data['label']["value"]] = None
+
+    json.dump(dict_new, to_write, ensure_ascii=False, indent=4)
