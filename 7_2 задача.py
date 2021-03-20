3a9637ca07a36a1d8068543ebd613ca1f37f7ba1
Виктория <ostrovskayap@bk.ru>
2021-03-20 15:49:44 +0700

    Сделаны задания Unittest

--- 7_2 задача.py
+++ 7_2 задача.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class SecondTask(unittest.TestCase):
     def test_random(self):
-        numbers = np.random.randint(1, 100, size=30)
+        numbers = np.random.random(30)
         for number in numbers:
             with self.subTest(number=number):
                 self.assertGreaterEqual(number, 0.5)
