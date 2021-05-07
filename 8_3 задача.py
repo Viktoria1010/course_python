1469b31b52fc4dbab5e2c8e89c83195ad3fdc42e
Виктория <ostrovskayap@bk.ru>
2021-05-07 15:18:33 +0700

    Сделаны задания collections (простите, пожалуйста, что с таким опозданием)

--- 8_3 задача.py
+++ 8_3 задача.py
@@ -3,7 +3,7 @@ import collections
 
 with open('C:\\Users\\Виктория\\Downloads\\RomeoAndJuliet.json', 'r', encoding='utf-8') as rajfile:
     romeo = json.load(rajfile)
-    cnt = collections.Counter(collections.defaultdict(int))
+    cnt = collections.Counter()
     for act in romeo["acts"]:
         for scene in act["scenes"]:
             for actions in scene["action"]:
