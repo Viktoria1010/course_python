f904f6fbb8b54fc8f7fc81063b9d48da3b83269e
Виктория <ostrovskayap@bk.ru>
2021-05-08 14:16:21 +0700

    Сделаны задания по инкапсуляции

--- 12_3 задача.py
+++ 12_3 задача.py
@@ -1,9 +1,12 @@
 class Train:
-    _password = 'icecream'
 
     def __init__(self, cost):
         self.cost = cost
         self.sale = 250
+        self._password = ''
+
+    def set_password(self, sign):
+        self._password = sign
 
     def __private(self):
         new_cost = self.cost - self.sale
@@ -11,13 +14,19 @@ class Train:
 
     def public(self):
         print(f'Цена билета составляет {self.cost}.')
-        parole = input('Введите пароль, если хотете получить скидку: ')
-        if parole == self._password:
-            self.__private()
+        if self._password != '':
+            parole = input('Введите пароль, если хотете получить скидку: ')
+            if parole == self._password:
+                self.__private()
+            else:
+                print(f'Не угадал. Цена билета составляет {self.cost}.')
         else:
-            print(f'Не угадал. Цена билета составляет {self.cost}.')
+            print('Нет пароля. Установите пароль.')
 
 
 first = Train(783)
 first.public()
-first.__private()
+first.set_password('hallelujah')
+first.public()
+
+
