493cbbe45e82d545e99208334893fef3b8857a08
Виктория <ostrovskayap@bk.ru>
2021-05-19 11:29:37 +0700

    Сделаны задания по наследованию

--- 9_2 задача 7 мая 15_20.py
+++ 9_2 задача 7 мая 15_20.py
@@ -1,12 +1,13 @@
 import time
 
-start_time = time.time()
-stroka = str(input())
-stroka = stroka.replace(" ", '')
-stroka = stroka.lower()
-stroka_1 = stroka[::-1]
-if stroka == stroka_1:
-    print('Палиндром')
-else:
-    print('Не палиндром')
-print('---%s seconds ---' % (time.time() - start_time))
+with open('time.txt', 'w') as to_write:
+    start_time = time.time()
+    stroka = str(input())
+    stroka = stroka.replace(" ", '')
+    stroka = stroka.lower()
+    stroka_1 = stroka[::-1]
+    if stroka == stroka_1:
+        print('Палиндром')
+    else:
+        print('Не палиндром')
+    to_write.write(str('%s seconds' % (time.time() - start_time)))
--- 9_3 задача.py
+++ 9_3 задача.py
@@ -7,7 +7,8 @@ birth = input('Введите дату рождения (гггг-мм-дд): ')
 birthdate = birth.split('-')
 how_much_days = 0
 if int(birthdate[1]) in range(1, 12):
-    if int(birthdate[2]) in range(1, 31):
+    if str(birthdate[2]) in {"01", "02", "03", "04", "05", "06", "07", "08", "09"}\
+            or int(birthdate[2]) in range(10, 31):
         date = time.strftime("%Y-%m-%d", current)
         years = (int(current_date[0]) - int(birthdate[0]))*365
         month = (int(current_date[1]) - int(birthdate[1]))*30
