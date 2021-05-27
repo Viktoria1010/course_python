0db01cc037ecf20542d0b44b50c7bb2214c5688e
Виктория <ostrovskayap@bk.ru>
2021-05-27 19:33:22 +0700

    Сделаны задания time

--- 9_2 задача.py
+++ 9_2 задача.py
@@ -1,13 +1,23 @@
 import time
+import os
 
-with open('time is 24_05_2021, 19_07.txt', 'w') as to_write:
-    start_time = time.time()
-    stroka = str(input())
-    stroka = stroka.replace(" ", '')
-    stroka = stroka.lower()
-    stroka_1 = stroka[::-1]
-    if stroka == stroka_1:
-        print('Палиндром')
-    else:
-        print('Не палиндром')
-    to_write.write(str('%s seconds' % (time.time() - start_time)))
+
+disk = 'C:\\'
+path = 'Users\\Виктория\\Desktop\\Для Универа\\Второй курс\\Четвертый семестр\\Прога\\course_python\\'
+file_name = str("the time is %s.txt" % (format(time.strftime("%Y_%m_%d-%H_%M_%S"))))
+whole_path = os.path.join(disk, path, file_name)
+start_time = time.time()
+
+
+def setup():
+    with open(whole_path, 'w') as file_to_write:
+        file_to_write.write(summa(10101010291938, 5678697083))
+
+
+def summa(a, b):
+    int(a) + int(b)
+    t = ' It took %s seconds to solve this' % (time.time() - start_time)
+    return t
+
+
+setup()
