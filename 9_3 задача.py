ebbb8b61e65e0f4acad10bb5463ab8e8c72603b3
Виктория <ostrovskayap@bk.ru>
2021-05-27 19:42:10 +0700

    Сделаны задания time

--- 9_3 задача.py
+++ 9_3 задача.py
@@ -6,7 +6,7 @@ current_date = date.split('-')
 birth = input('Введите дату рождения (гггг-мм-дд): ')
 birthdate = birth.split('-')
 how_much_days = 0
-if int(birthdate[1]) in range(1, 12):
+if int(birthdate[1]) in range(1, 13):
     if str(birthdate[2]) in {"01", "02", "03", "04", "05", "06", "07", "08", "09"}\
             or int(birthdate[2]) in range(10, 31):
         date = time.strftime("%Y-%m-%d", current)
