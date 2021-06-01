68f69d0092b15449dd1a20ab1009f2c27ee2b8a0
Виктория <ostrovskayap@bk.ru>
2021-06-01 19:10:47 +0700

    Сделаны задания TF-IDF

--- task_14.py
+++ task_14.py
@@ -1,37 +1,65 @@
 import os
-import collections
 import math
 import json
+import re
 
 
 class TFIDF:
-    def __init__(self, inp_doc, docs):
-        self.__tf = {word: self.tf(word, inp_doc) for word in inp_doc}
-        if os.path.exists('idf_file.js'):
-            with open('idf_file.js', 'r') as idf:
-                self.__idf = json.load(idf)
+    def __init__(self, path_to_file, texts):
+        if os.path.exists(path_to_file):
+            with open('idf.json', 'r') as idf_file:
+                self._idf = json.load(idf_file)
         else:
-            self.__idf = {word: self.idf(word, docs) for doc in docs for word in doc}
-            with open('idf_file.js', 'w') as idfout:
-                json.dump(self.__idf, idfout)
+            self._idf = self._count_idf(texts)
 
-    def tf(self, word, doc):
-        return collections.Counter(doc).get(word)/len(doc)
+    def _count_tf(self, texts):
+        tf_output = {}
+        list_of_words = re.findall('[a-zа-яё]+', texts, flags=re.IGNORECASE)
+        for word in list_of_words:
+            amount = list_of_words.count(word)
+            tf = amount/len(list_of_words)
+            tf_output[word] = tf
+        return tf_output
 
-    def idf(self, word, docs):
-        docs_c = 0
-        for doc in docs:
-            if word in doc:
-                docs_c += 1
-        return math.log(len(docs)/docs_c)
+    def get_tf(self, texts):
+        return self._count_tf(texts)
 
-    def tf_idf(self, words):
-        result = dict()
-        for word in words:
-            _tf = self.tf(word, words)
-            try:
-                _idf = self.__idf[word]
-            except KeyError:
-                _idf = 0
-            result[word] = _tf*_idf
-        return result
+    def _count_idf(self, texts):
+        with open(texts, 'r', encoding='utf-8') as file_for_tfidf:
+            text = []
+            for line in file_for_tfidf:
+                text.append(line)
+            idf_output = {}
+            big_list = []
+            for t in text:
+                list_of_words = re.findall('[a-zа-яё]+', t, flags=re.IGNORECASE)
+                big_list.append(list_of_words)
+            if len(big_list) == 1:
+                for wo in list_of_words:
+                    idf = math.log10(len(list_of_words) / sum([1.0 for i in list_of_words if wo in i]))
+                    idf_output[wo] = idf
+            else:
+                for w in big_list:
+                    for word in w:
+                        idf = math.log10(len(big_list)/sum([1.0 for i in big_list if word in i]))
+                        idf_output[word] = idf
+            with open('idf.json', 'w') as idf_file:
+                json.dump(idf_output, idf_file, indent=4, ensure_ascii=False)
+            return idf_output
+
+    def get_idf(self, texts):
+        return self._count_idf(texts)
+
+    def tfidf(self, texts):
+        tf_idf_output = {}
+        for key in self.get_tf(texts):
+            tfidf = self.get_idf(texts)[key] * self._idf[key]
+            tf_idf_output[key] = tfidf
+        return tf_idf_output
+
+
+tf_idf = TFIDF(r'C:\Users\Виктория\OneDrive\Рабочий стол\Для Универа\Второй курс\Четвертый семестр\Прога\course_python\idf.json', 'text.txt')
+tf_idf.tfidf('Великолепная «Школа злословия» вернулась в эфир после летних каникул в новом формате.')
+
+
+
